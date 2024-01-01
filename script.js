63811fc516112f45f006af9fdc05830c1dcad560
Alok Mourya <122997111+Akmourya24@users.noreply.github.com>
2024-01-01 23:31:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("btn1").addEventListener("click",login);
+function login(){
+    let x=document.getElementById("mail");
+    x=alert("Your are login Netflix")
+}
+ let A= document.getElementById('btn');
+ A=addEventListener("click", clickme);
+ A=function clickme(){
+    document.getElementById('btn').innerHTMl.href="login_page.html"
+ }
+  
+
